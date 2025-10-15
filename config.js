5450d4ba21c340c116c6e7cf4565665c28cd9ef9
Zilong <92193735+al1103@users.noreply.github.com>
2025-10-15 08:59:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const TELEGRAM_BOT_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN';
-export const TELEGRAM_CHAT_ID = 'YOUR_CHAT_ID';
+export const TELEGRAM_BOT_TOKEN = '8454938284:AAFbEQIq1yzTX2GBaV6ukOepBYIOAyXmn0I';
+export const TELEGRAM_CHAT_ID = 'get_device_bot';
 export const TELEGRAM_API_URL = 'https://api.telegram.org';
 export const IP_LOOKUP_URL = 'https://ipapi.co/json/';
